1d3f0eed45a8a844d9fe1d5f4b789f6f81914a74
Anushk Jain <anushk2201@gmail.com>
2024-07-23 11:41:13 +0530

    feat: added incredients in the final order list.

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -2,12 +2,13 @@
 import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
 import { createOrder } from "../../services/apiRestaurant";
 import Button from "../../ui/Button";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { clearCart, getCart, getTotalCartPrice } from "../cart/CartSlice";
 import EmptyCart from "../cart/EmptyCart";
 import store from "../../store";
 import { formatCurrency } from "../../utils/helpers";
 import { useState } from "react";
+import { fetchAddress } from "../user/userSlice";
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -17,8 +18,16 @@ const isValidPhone = (str) =>
 
 function CreateOrder() {
 	const navigation = useNavigation();
+	const dispatch = useDispatch();
 	const isSubmitting = navigation.state === "submitting";
-	const username = useSelector(state => state.user.username);
+	const {
+		username,
+		status: addressStatus,
+		position,
+		address,
+		error: errorAddress,
+	} = useSelector((state) => state.user);
+	const isLoadingAddress = addressStatus === "loading";
 
 	const formErrors = useActionData();
 	const [withPriority, setWithPriority] = useState(false);
@@ -28,81 +37,139 @@ function CreateOrder() {
 	const priorityPrice = withPriority ? totalCartPrice * 0.2 : 0;
 	const totalPrice = totalCartPrice + priorityPrice;
 
-	if(!cart.length) return <EmptyCart />;
+	if (!cart.length) return <EmptyCart />;
 
 	return (
-		<div className="px-4 py-6">
-			<h2 className="text-xl font-semibold mb-8">Ready to order? Let&apos;s go!</h2>
+		<div className='px-4 py-6'>
+			<h2 className='text-xl font-semibold mb-8'>
+				Ready to order? Let&apos;s go!
+			</h2>
 
-			<Form method="POST">
-				<div className="mb-5 flex gap-2 flex-col sm:flex-row sm:items-center">
-					<label className="sm:basis-40">First Name</label>
-					<input type='text' name='customer' defaultValue={username} required className="input grow" />
+			<Form method='POST'>
+				<div className='mb-5 flex gap-2 flex-col sm:flex-row sm:items-center'>
+					<label className='sm:basis-40'>First Name</label>
+					<input
+						type='text'
+						name='customer'
+						defaultValue={username}
+						required
+						className='input grow'
+					/>
 				</div>
 
-				<div className="mb-5 flex gap-2 flex-col sm:flex-row sm:items-center">
-					<label className="sm:basis-40">Phone number</label>
-					<div className="grow">
-						<input type='tel' name='phone' required className="input w-full" />
-						{formErrors?.phone && <p className="text-xs mt-2 text-red-700 bg-red-100 p-2 rounded-md">{formErrors.phone}</p>}
+				<div className='mb-5 flex gap-2 flex-col sm:flex-row sm:items-center'>
+					<label className='sm:basis-40'>Phone number</label>
+					<div className='grow'>
+						<input
+							type='tel'
+							name='phone'
+							required
+							className='input w-full'
+						/>
+						{formErrors?.phone && (
+							<p className='text-xs mt-2 text-red-700 bg-red-100 p-2 rounded-md'>
+								{formErrors.phone}
+							</p>
+						)}
 					</div>
-
-					
 				</div>
 
-				<div className="mb-5 flex gap-2 flex-col sm:flex-row sm:items-center">
-					<label className="sm:basis-40">Address</label>
-					<div className="grow"> 
-						<input type='text' name='address' required className="input w-full" />
-					</div> 
+				<div className='mb-5 flex gap-2 flex-col sm:flex-row sm:items-center relative'>
+					<label className='sm:basis-40'>Address</label>
+					<div className='grow'>
+						<input
+							type='text'
+							name='address'
+							disabled={isLoadingAddress}
+							required
+							className='input w-full'
+							defaultValue={address}
+						/>
+						{addressStatus === "error" && (
+							<p className='text-xs mt-2 text-red-700 bg-red-100 p-2 rounded-md'>
+								{errorAddress}
+							</p>
+						)}
+					</div>
+					{!position.latitude && !position.longitude && (
+						<span className='absolute right-[3px] top-[2px] md:right-[5px] md:top-[5px] z-50'>
+							<Button
+								type='small'
+								onClick={(e) => {
+									e.preventDefault();
+									dispatch(fetchAddress());
+								}}
+								disabled={isLoadingAddress}
+							>
+								Get position
+							</Button>
+						</span>
+					)}
 				</div>
 
-				<div className="mb-12 flex gap-5 items-center">
+				<div className='mb-12 flex gap-5 items-center'>
 					<input
 						type='checkbox'
 						name='priority'
 						id='priority'
-						className="h-6 w-6 accent-yellow-400 focus:outline-none focus:ring focus:ring-yellow-400 focus:ring-offset-2"
+						className='h-6 w-6 accent-yellow-400 focus:outline-none focus:ring focus:ring-yellow-400 focus:ring-offset-2'
 						value={withPriority}
 						onChange={(e) => setWithPriority(e.target.checked)}
 					/>
-					<label htmlFor='priority' className="font-medium">
+					<label htmlFor='priority' className='font-medium'>
 						Want to yo give your order priority?
 					</label>
 				</div>
 
 				<div>
-					<input type="hidden" name="cart" value={JSON.stringify(cart)} />
-					<Button type="primary" disabled={isSubmitting}>{isSubmitting ? 'Placing order...' : `Order now from ${formatCurrency(totalPrice)}`}</Button>
+					<input
+						type='hidden'
+						name='cart'
+						value={JSON.stringify(cart)}
+					/>
+
+					<input
+						type='hidden'
+						name='position'
+						value={position.longitude && position.latitude ? `${position.latitude}, ${position.longitude}` : ''}
+					/>
+					<Button
+						type='primary'
+						disabled={isSubmitting || isLoadingAddress}
+					>
+						{isSubmitting
+							? "Placing order..."
+							: `Order now from ${formatCurrency(totalPrice)}`}
+					</Button>
 				</div>
 			</Form>
 		</div>
 	);
 }
 
-export const action = async ({request}) => {
+export const action = async ({ request }) => {
 	const formData = await request.formData();
 	const data = Object.fromEntries(formData);
 
 	const order = {
 		...data,
 		cart: JSON.parse(data.cart),
-        priority: data.priority === "true",
+		priority: data.priority === "true",
+	};
+
+	const errors = {};
+	if (!isValidPhone(order.phone))
+		errors.phone = "Please enter a correct phone number";
+
+	if (Object.keys(errors).length > 0) {
+		return errors;
 	}
-
-	const errors = {}
-	if(!isValidPhone(order.phone)) errors.phone = 'Please enter a correct phone number';
-
-	if(Object.keys(errors).length > 0) {
-        return errors;
-    }
 
 	const newOrder = await createOrder(order);
 
 	store.dispatch(clearCart());
 
 	return redirect(`/order/${newOrder.id}`);
-
-}
+};
 
 export default CreateOrder;
--- src/features/order/Order.jsx
+++ src/features/order/Order.jsx
@@ -1,15 +1,23 @@
 // Test ID: IIDSAT
-import OrderItem from "./OrderItem"
-import { useLoaderData } from "react-router-dom";
+import OrderItem from "./OrderItem";
+import { useFetcher, useLoaderData } from "react-router-dom";
 import { getOrder } from "../../services/apiRestaurant";
 import {
 	calcMinutesLeft,
 	formatCurrency,
 	formatDate,
 } from "../../utils/helpers";
+import { useEffect } from "react";
 
 function Order() {
 	const order = useLoaderData();
+
+	const fetcher = useFetcher();
+	useEffect(() => {
+		if (!fetcher.data && fetcher.state === "idle") fetcher.load("/menu");
+	}, [fetcher]);
+
+	console.log(fetcher.data);
 	// Everyone can search for all orders, so for privacy reasons we're gonna gonna exclude names or address, these are only for the restaurant staff
 	const {
 		id,
@@ -51,11 +59,19 @@ function Order() {
 					(Estimated delivery: {formatDate(estimatedDelivery)})
 				</p>
 			</div>
-			
-			<ul className="divide-y divide-stone-200 border-b border-t">
+
+			<ul className='divide-y divide-stone-200 border-b border-t'>
 				{cart.map((item) => (
-					<OrderItem item={item} key={item.pizzaId} />
-                ))}
+					<OrderItem
+						item={item}
+						key={item.pizzaId}
+						isLoadingIngredients={fetcher.state === "loading"}
+						ingredients={
+							fetcher?.data?.find((el) => el.id === item.pizzaId)
+								?.ingredients ?? []
+						}
+					/>
+				))}
 			</ul>
 
 			<div className='space-y-2 bg-stone-200 px-6 py-5'>
